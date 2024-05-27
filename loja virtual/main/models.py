4f9c665824c43d944438f5e31cd7c00d23cf6518
AurélioMSFilho <aureliofilhotj@gmail.com>
2024-05-27 15:04:52 -0300

    primeiro commit

--- loja virtual/main/models.py
+++ loja virtual/main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 # Create your models here.
 
-
+TAMANHOS = (('P', 'Pequeno'), ('M', 'Médio'), ('G', 'Grande'))
 class Categoria(models.Model):
     nome = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True, db_index=True)
@@ -13,10 +13,12 @@ class Categoria(models.Model):
 
 
 class Produto(models.Model):
+    categoria = models.ForeignKey(Categoria, related_name='produtos', null=True, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
     descricao = models.TextField(blank=True)
-    preco = models.DecimalField(max_length=10, decimal_places=2)
+    tamanho = models.CharField(choices=TAMANHOS, max_length=1)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
     disponivel = models.BooleanField(default=True)
     estoque = models.PositiveIntegerField()
     data_criacao = models.DateTimeField(auto_now_add=True)
@@ -24,5 +26,41 @@ class Produto(models.Model):
     imagem = models.ImageField(upload_to='imagens-produtos', blank=True)
 
     def __str__(self):
-        return  self.nome
+        return self.nome
 
+
+class Loja(models.Model):
+    nome = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=30)
+    uf = models.CharField(max_length=2)
+    cep = models.CharField(max_length=8)
+    email = models.EmailField()
+    produtos = models.ManyToManyField(Produto, blank=True)
+
+
+class Endereco(models.Model):
+    logradouro = models.CharField(max_length=50)
+    numero = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=20)
+    cidade = models.CharField(max_length=30)
+    uf = models.CharField(max_length=2)
+    cep = models.CharField(max_length=8)
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=100)
+
+    endereco = models.OneToOneField(Endereco, on_delete=models.CASCADE, primary_key=True)
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=50)
+    email = models.EmailField()
+    gerente = models.ForeignKey('self', on_delete=models.CASCADE)
+
+class Conta(models.Model):
+    descricao = models.CharField(max_length=100)
+    saldo = models.FloatField()
+    superior = models.ForeignKey('self', on_delete=models.CASCADE)
